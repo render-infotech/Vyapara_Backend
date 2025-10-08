33a14d813c4e575fa16ed0c9602b5615610d619e
shubhamRender <shubham.render@gmail.com>
2025-10-08 13:46:51 +0530

    minor changes in the package versions

--- src/utils/database.js
+++ src/utils/database.js
@@ -12,21 +12,16 @@ const dbConfig = {
 };
 
 // Create Sequelize instance
-export const sequelize = new Sequelize(
-  dbConfig.database,
-  dbConfig.username,
-  dbConfig.password,
-  {
-    host: dbConfig.host,
-    dialect: dbConfig.dialect,
-    port: dbConfig.port,
-    dialectModule: mysql2,
-    timezone: '+00:00', // Use UTC
-    dialectOptions: {
-      timezone: 'Z', // This is for reading from the database
-    },
-  }
-);
+export const sequelize = new Sequelize(dbConfig.database, dbConfig.username, dbConfig.password, {
+  host: dbConfig.host,
+  dialect: dbConfig.dialect,
+  port: dbConfig.port,
+  dialectModule: mysql2,
+  timezone: '+00:00', // Use UTC
+  dialectOptions: {
+    timezone: 'Z', // This is for reading from the database
+  },
+});
 
 // Define a generic function for executing SQL queries
 export const query = async (sql, values, callback) => {
