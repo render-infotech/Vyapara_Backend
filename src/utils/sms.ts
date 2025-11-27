b8777dcc5decf5633db61e23b702dd8de2040351
Afridi <mohammed.afrid@renderinfotech.com>
2025-11-27 15:21:58 +0530

    Fixed : ESlint

--- src/utils/sms.ts
+++ src/utils/sms.ts
@@ -8,33 +8,34 @@ import logger from './logger';
 
 // const client = twilio(accountSid, authToken);
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
 export const sendSms = async (to: string, body: string) => {
-    try {
-        // if (!accountSid || !authToken || !fromNumber) {
-        //     logger.error('Twilio credentials missing');
-        //     return false;
-        // }
+  try {
+    // if (!accountSid || !authToken || !fromNumber) {
+    //   logger.error('Twilio credentials missing');
+    //   return false;
+    // }
 
-        // const message = await client.messages.create({
-        //     body,
-        //     from: fromNumber,
-        //     to,
-        // });
+    // const message = await client.messages.create({
+    //   body,
+    //   from: fromNumber,
+    //   to,
+    // });
 
-        // logger.info(`SMS sent successfully to ${to}: ${message.sid}`);
-        return true;
-    } catch (error) {
-        logger.error(`Error sending SMS to ${to}:`, error);
-        return false;
-    }
+    // logger.info(`SMS sent successfully to ${to}: ${message.sid}`);
+    return true;
+  } catch (error) {
+    logger.error(`Error sending SMS to ${to}:`, error);
+    return false;
+  }
 };
 
 export const generateNumericOtp = (length = 6): string => {
-    const min = Math.pow(10, length - 1);
-    const max = Math.pow(10, length) - 1;
-    return crypto.randomInt(min, max + 1).toString();
+  const min = Math.pow(10, length - 1);
+  const max = Math.pow(10, length) - 1;
+  return crypto.randomInt(min, max + 1).toString();
 };
 
 export const hashOtp = (otp: string): string => {
-    return crypto.createHash('sha256').update(otp).digest('hex');
+  return crypto.createHash('sha256').update(otp).digest('hex');
 };
